c5e77a043d9fcba06ed83acbed6baa2c34d09301
Dwayne Macgowan <dwaynemac@gmail.com>
2015-02-23 23:45:06 -0300

    Add PadmaAccount.find_by_nucleo_id
    client to v0/accounts/by_nucleo_id END POINT

--- app/models/padma_account.rb
+++ app/models/padma_account.rb
@@ -47,6 +47,26 @@ class PadmaAccount < LogicalModel
     :account
   end
 
+  def self.find_by_nucleo_id(nucleo_id, params={})
+    res = nil
+    params = self.merge_key(params)
+    request = Typhoeus::Request.new("#{resource_uri}/by_nucleo_id/#{nucleo_id}", params: params )
+
+    request.on_complete do |response|
+      if response.code >= 200 && response.code < 400
+        log_ok(response)
+        res = self.new.from_json(response.body)
+      else
+        log_failed(response)
+      end
+    end
+
+    self.hydra.queue(request)
+    self.hydra.run
+
+    res
+  end
+
   private
 
   # Accounts-ws uses account name of identification
