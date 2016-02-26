8c24d13384867150d874040e37cdcc1ac9d9053e
Dwayne Macgowan <dwaynemac@gmail.com>
2016-02-26 15:34:11 -0300

    make a model for apikey

---
+++ app/models/api_key.rb
@@ -0,0 +1,17 @@
+class ApiKey < LogicalModel
+  use_hydra Accounts::HYDRA
+  set_resource_host Accounts::HOST
+  set_resource_path "/v0/api_keys"
+
+  attribute :account_name
+  attribute :access
+
+  set_api_key 'token', Accounts::API_KEY
+
+  TIMEOUT = 5500 # milisecons
+  PER_PAGE = 9999
+
+  def json_root
+    'api_key'
+  end
+end
