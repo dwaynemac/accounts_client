541c417b1832269324161aa1e9996a7324717834
Alex Falke <afalkear@gmail.com>
2018-09-28 19:49:33 -0300

    update rspec

--- lib/accounts/shared_examples.rb
+++ lib/accounts/shared_examples.rb
@@ -1,7 +1,7 @@
 shared_examples_for "it belongs to an account"  do
   it { should validate_presence_of :account_name }
   it "should return account on #account" do
-    PadmaAccount.should_receive(:find).with(object.account_name).and_return(PadmaAccount.new(account_name: object.account_name))
+    expect(PadmaAccount).to receive(:find).with(object.account_name).and_return(PadmaAccount.new(account_name: object.account_name))
     object.account.should be_a(PadmaAccount)
   end
 end
@@ -9,7 +9,7 @@ end
 shared_examples_for "it belongs to a user"  do
   it { should validate_presence_of :username}
   it "should return user on #user" do
-    PadmaUser.stub(:find).with(object.username).and_return(PadmaUser.new(username: object.username))
+    expect(PadmaUser).to receive(:find).with(object.username).and_return(PadmaUser.new(username: object.username))
     object.user.should be_a(PadmaUser)
   end
   it "should use Rails.cache"
