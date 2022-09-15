5da7b2e3cebc9491c2f78ddc7db7a05579aa5509
Dwayne Macgowan <dwaynemac@gmail.com>
2022-09-15 09:40:39 -0300

    use apikey /check instead of /show

--- app/models/api_key.rb
+++ app/models/api_key.rb
@@ -13,6 +13,31 @@ class ApiKey < LogicalModel
   TIMEOUT = 5500 # milisecons
   PER_PAGE = 9999
 
+  def self.find(id, params={})
+    Rails.logger.warn "ApiKey.find is deprecated. Use ApiKey.check instead"
+    self.check(id, params)
+  end
+
+  def self.check(key, params={})
+    res = nil
+    params = self.merge_key(params).merge({api_key: key})
+    request = Typhoeus::Request.new("#{resource_uri}/check", params: params )
+
+    request.on_complete do |response|
+      if response.code >= 200 && response.code < 400
+        log_ok(response)
+        res = self.new.from_json(response.body)
+      else
+        log_failed(response)
+      end
+    end
+
+    self.hydra.queue(request)
+    self.hydra.run
+
+    res
+  end
+
   def json_root
     'api_key'
   end
